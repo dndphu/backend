847281a230c46db80baa1ffc58eb682174156bed
phudnd <38520225+dndphu@users.noreply.github.com>
2023-09-19 00:11:35 +0700

    feat: add route

--- index.js
+++ index.js
@@ -2,17 +2,20 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT|| 3000;
 const dotenv = require("dotenv").config();
-const mongoose = require("mongoose")
-
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+//config router
+const route = require("./routers")
+route(app)
 
+//connect mongodb
 const db = require("./config/db");
 db.connect();
 
+app.use(express.json());
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
