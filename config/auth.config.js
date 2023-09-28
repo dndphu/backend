ed37aec984b83fb61a46e00dbd9cca2b6134ef43
phudnd <38520225+dndphu@users.noreply.github.com>
2023-09-29 00:24:50 +0700

    fix: fix typo dev

--- config/auth.config.js
+++ config/auth.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   secret: process.env.TOKEN_SECRET,
-  // jwtExpiration: 3600, // 1 hour
-  // jwtRefreshExpiration: 86400, // 24 hours
+  jwtExpiration: 3600, // 1 hour
+  jwtRefreshExpiration: 86400, // 24 hours
 
   /* for test */
-  jwtExpiration: 10,          // 1m
-  jwtRefreshExpiration: 20,  // 2 minutes
+  // jwtExpiration: 10,          // 1m
+  // jwtRefreshExpiration: 20,  // 2 minutes
 };
